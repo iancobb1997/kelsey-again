c259d4056ad341d0b137862e4dd7d7109c090df5
Ian Cobb <ian.cobb1997@gmail.com>
2017-12-16 18:35:13 -0700

    initial

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,25 @@
+module ApplicationHelper
+
+  def nav_items
+    [
+      {
+        url: pictures_path,
+        title: 'Pictures'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{ active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+end
